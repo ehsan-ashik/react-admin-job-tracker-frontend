404b474b04c9b90f284a84ac4575109a1f527476
Ehsan Ul Haque <16237525+ehsan-ashik@users.noreply.github.com>
2025-01-25 02:07:40 -0500

    Blob fetch api and Resume PDF View added

---
+++ api/blob.js
@@ -0,0 +1,33 @@
+import { AccountSASPermissions, AccountSASResourceTypes, AccountSASServices, generateAccountSASQueryParameters, SASProtocol, StorageSharedKeyCredential } from '@azure/storage-blob';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const constants = {
+    accountName: process.env.AZURE_ACCOUNT_NAME,
+    accountKey: process.env.AZURE_ACCESS_KEY
+};
+const sharedKeyCredential = new StorageSharedKeyCredential(
+    constants.accountName ? constants.accountName : "",
+    constants.accountKey ? constants.accountKey : ""
+);
+
+export function createAccountSas() {
+    const sasOptions = {
+
+        services: AccountSASServices.parse("b").toString(),          // blobs, tables, queues, files
+        resourceTypes: AccountSASResourceTypes.parse("co").toString(), // service, container, object
+        permissions: AccountSASPermissions.parse("r"),          // permissions
+        protocol: SASProtocol.HttpsAndHttp,
+        startsOn: new Date(new Date().valueOf() - (2 * 60 * 1000)) , // 2 minutes
+        expiresOn: new Date(new Date().valueOf() + (26 * 60 * 60 * 1000)),   // 26 hours
+    };
+
+    const sasToken = generateAccountSASQueryParameters(
+        sasOptions,
+        sharedKeyCredential 
+    ).toString();
+
+    // prepend sasToken with `?`
+    return (sasToken[0] === '?') ? sasToken : `?${sasToken}`;
+}
---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var cors = require('cors')
+const { createAccountSas } = require('./blob')
+const app = express()
+const port = 5000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    const sasToken = createAccountSas()
+    res.send({sasToken})
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
