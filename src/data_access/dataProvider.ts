9e69497864bc07b2206e2945c9c670f21c8b3e28
Ehsan Ul Haque <16237525+ehsan-ashik@users.noreply.github.com>
2025-01-26 00:25:44 -0500

    Resume Create and file upload

--- src/data_access/dataProvider.ts
+++ src/data_access/dataProvider.ts
@@ -1,11 +1,36 @@
-import { DataProvider, fetchUtils } from "react-admin";
+import { CreateParams, DataProvider, fetchUtils } from "react-admin";
 
 const API_URL = "http://localhost:3000/api";
 
+type ResumeParams = {
+  id: string;
+  title: string;
+  remark: string;
+  resume: {
+    rawFile: File;
+    src?: string;
+    title?: string;
+  };
+};
+
+const createResumeFormData = (params: CreateParams<ResumeParams>) => {
+  const formData = new FormData();
+  params.data.resume?.rawFile &&
+    formData.append("file", params.data.resume.rawFile);
+  params.data.title && formData.append("title", params.data.title);
+  params.data.remark && formData.append("remark", params.data.remark);
+
+  return formData;
+};
+
 export const dataProvider: DataProvider = {
   getList: async (resource, params) => {
-    const { page, perPage } = params.pagination !== undefined ? params.pagination : {page: 1, perPage: 10};
-    const { field, order } = params.sort !== undefined ? params.sort: {field: "id", order: "ASC"};
+    const { page, perPage } =
+      params.pagination !== undefined
+        ? params.pagination
+        : { page: 1, perPage: 10 };
+    const { field, order } =
+      params.sort !== undefined ? params.sort : { field: "id", order: "ASC" };
     const filter = JSON.stringify(params.filter);
 
     const response = await fetchUtils.fetchJson(
@@ -13,26 +38,24 @@ export const dataProvider: DataProvider = {
     );
     return {
       data: response.json.data,
-      total: parseInt(response.headers.get("X-Total-Rows") || '10', 10),
+      total: parseInt(response.headers.get("X-Total-Rows") || "10", 10),
     };
   },
   getOne: async (resource, params) => {
     const id = params.id;
-    const response = await fetchUtils.fetchJson(
-      `${API_URL}/${resource}/${id}`,
-    );
+    const response = await fetchUtils.fetchJson(`${API_URL}/${resource}/${id}`);
     return {
-      data: response.json.data
-    }
+      data: response.json.data,
+    };
   },
   getMany: async (resource, params) => {
-    const ids = params.ids.join(",")
+    const ids = params.ids.join(",");
     const response = await fetchUtils.fetchJson(
       `${API_URL}/${resource}?filter={"id":[${ids}]}`,
     );
     return {
-      data: response.json.data
-    }
+      data: response.json.data,
+    };
   },
   getManyReference: async (resource, params) => {
     const { page, perPage } = params.pagination;
@@ -42,52 +65,63 @@ export const dataProvider: DataProvider = {
     const response = await fetchUtils.fetchJson(
       `${API_URL}/${resource}?page=${page}&limit=${perPage}&sort=["${field}","${order}"]&filter=${filter}`,
     );
-    
+
     return {
       data: response.json.data,
-      total: parseInt(response.headers.get("X-Total-Rows") || '10', 10),
+      total: parseInt(response.headers.get("X-Total-Rows") || "10", 10),
     };
   },
   create: async (resource, params) => {
+    if (resource == "resume") {
+      const formData = createResumeFormData(params);
+      const resumeResponse = await fetchUtils.fetchJson(
+        `${API_URL}/${resource}`,
+        {
+          method: "POST",
+          body: formData,
+        },
+      );
+      return { data: resumeResponse.json.data };
+    }
     const response = await fetchUtils.fetchJson(`${API_URL}/${resource}`, {
-        method: 'POST',
-        body: JSON.stringify(params.data),
-    })
+      method: "POST",
+      body: JSON.stringify(params.data),
+    });
     return { data: response.json.data };
   },
   update: async (resource, params) => {
     const url = `${API_URL}/${resource}/${params.id}`;
     const response = await fetchUtils.fetchJson(url, {
-        method: 'PUT',
-        body: JSON.stringify(params.data),
-    })
+      method: "PUT",
+      body: JSON.stringify(params.data),
+    });
     return { data: response.json.data };
   },
   updateMany: async (resource, params) => {
     const query = {
-        filter: JSON.stringify({ id: params.ids}),
+      filter: JSON.stringify({ id: params.ids }),
     };
     const url = `${API_URL}/${resource}?${JSON.stringify(query)}`;
     const { json } = await fetchUtils.fetchJson(url, {
-        method: 'PUT',
-        body: JSON.stringify(params.data),
-    })
+      method: "PUT",
+      body: JSON.stringify(params.data),
+    });
     return { data: json };
   },
   delete: async (resource, params) => {
     const url = `${API_URL}/${resource}/${params.id}`;
     const response = await fetchUtils.fetchJson(url, {
-      method: 'DELETE'
-    })
+      method: "DELETE",
+    });
     return { data: response.json.data };
   },
   deleteMany: async (resource, params) => {
     const query = {
-        filter: JSON.stringify({ id: params.ids}),
+      filter: JSON.stringify({ id: params.ids }),
     };
     const url = `${API_URL}/${resource}?${JSON.stringify(query)}`;
     const response = await fetchUtils.fetchJson(url, {
-        method: 'DELETE',
+      method: "DELETE",
     });
     return { data: response.json.data };
   },
